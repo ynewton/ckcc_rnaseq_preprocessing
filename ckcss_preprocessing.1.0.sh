88c7bcddcc1bb3d4df34d77f8c22130e394162ed
ynewton <yulia.newton@gmail.com>
2016-08-22 20:03:17 -0700

    fixed a bug

--- ckcss_preprocessing.1.0.sh
+++ ckcss_preprocessing.1.0.sh
@@ -20,9 +20,11 @@ python2.7 /usr/bin/aggregate_dups.py --in_file $1.2decimals --out_file temp.tab
 
 echo "Aggregating duplicate samples ..."
 /usr/bin/./transpose.pl temp.tab > temp_t.tab
-python2.7 /usr/bin/aggregate_dups.py --in_file temp_t.tab --out_file $1.2decimals.no_dups
+python2.7 /usr/bin/aggregate_dups.py --in_file temp_t.tab --out_file temp_t2.tab
+/usr/bin/./transpose.pl temp_t2.tab > $1.2decimals.no_dups
 rm temp.tab
 rm temp_t.tab
+rm temp_t2.tab
 
 echo "Apply expression level filter ..."
 python2.7 /usr/bin/filter_out_genes_unexpressed_in_most_samples.py --in_file $1.2decimals.no_dups --proportion_unexpressed 0.8 --out_file $1.2decimals.no_dups.expression_filter_0.8
