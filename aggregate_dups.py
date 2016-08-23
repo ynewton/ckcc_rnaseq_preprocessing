a085ce38668a7562a846cb1a2e185b70dcf5c26b
ynewton <yulia.newton@gmail.com>
2016-08-22 18:37:57 -0700

    added duplicate aggregation

---
+++ aggregate_dups.py
@@ -0,0 +1,54 @@
+#Yulia Newton
+#python2.7 aggregate_dups.py --in_file input.tab --out_file output.tab
+
+import optparse, sys, os, glob, numpy, operator, math
+parser = optparse.OptionParser()
+parser.add_option("--in_file", dest="in_file", action="store", default="", help="")
+parser.add_option("--out_file", dest="out_file", action="store", default="", help="")
+opts, args = parser.parse_args()
+
+#process input arguments:
+in_file = opts.in_file
+out_file_name = opts.out_file
+
+print >> sys.stderr, "Reading in input..."
+line_count = 1
+feature_val_dict = {}
+feature_count_dict = {}
+input = open(in_file, 'r')
+for line in input:
+	line = line.replace("\n", "")
+	if line_count == 1:
+		header_line = line
+	else:
+		line_elems = line.split("\t")
+		if not(line_elems[0] in feature_val_dict):
+			feature_val_dict[line_elems[0]] = []
+		feature_val_dict[line_elems[0]].append([float(x) for x in line_elems[1:]])
+		
+		if not(line_elems[0] in feature_count_dict):
+			feature_count_dict[line_elems[0]] = 0
+		feature_count_dict[line_elems[0]] += 1
+		
+	line_count += 1
+
+input.close()
+
+print >> sys.stderr, "Outputting and aggregating duplicates..."
+output = open(out_file_name, 'w')
+print >> output, "ID\t"+header_line
+for k in feature_count_dict.keys():
+	k_count = feature_count_dict[k]
+	if k_count == 1:	#not duplicated
+		output_vals = [str(x) for x in feature_val_dict[k]]
+	
+	else:
+		k_vals_t = numpy.transpose(feature_val_dict[k])
+		aggregated_vals = []
+		for i in range(len(k_vals_t)):
+			aggregated_vals.append(numpy.median(k_vals_t[i]))
+		output_vals = [str(x) for x in aggregated_vals]
+		
+	print  >> output, k + "\t" + "\t".join(output_vals)
+
+output.close()
--- ckcss_preprocessing.1.0.sh
+++ ckcss_preprocessing.1.0.sh
@@ -15,8 +15,17 @@ echo "Apply CKCC preprocessing pipeline v1.0 to $1"
 echo "Applying number rounding and truncation ..."
 python2.7 /usr/bin/round_digits.py --in_matrix $1 --num_digits 2 --out_matrix $1.2decimals
 
+echo "Aggregating duplicate features ..."
+python2.7 /usr/bin/aggregate_dups.py --in_file $1.2decimals --out_file temp.tab
+
+echo "Aggregating duplicate samples ..."
+/usr/bin/./transpose.pl temp.tab > temp_t.tab
+python2.7 /usr/bin/aggregate_dups.py --in_file temp_t.tab --out_file $1.2decimals.no_dups
+rm temp.tab
+rm temp_t.tab
+
 echo "Apply expression level filter ..."
-python2.7 /usr/bin/filter_out_genes_unexpressed_in_most_samples.py --in_file $1.2decimals --proportion_unexpressed 0.8 --out_file $1.2decimals.expression_filter_0.8
+python2.7 /usr/bin/filter_out_genes_unexpressed_in_most_samples.py --in_file $1.2decimals.no_dups --proportion_unexpressed 0.8 --out_file $1.2decimals.no_dups.expression_filter_0.8
 
 echo "Applying variance filter ..."
-python2.7 /usr/bin/filter_out_lowest_varying_genes.py --in_file $1.2decimals.expression_filter_0.8 --filter_level .2 --out_file $1.2decimals.expression_filter_0.8.variance_filter_0.2
+python2.7 /usr/bin/filter_out_lowest_varying_genes.py --in_file $1.2decimals.no_dups.expression_filter_0.8 --filter_level .2 --out_file $1.2decimals.no_dups.expression_filter_0.8.variance_filter_0.2
